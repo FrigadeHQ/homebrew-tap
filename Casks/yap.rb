dd6a58b5fe08f8138a2ee34c8a8cdcde7c23df9a
christianmat <christian_mathiesen@alumni.brown.edu>
2026-07-24 12:42:25 -0700

    Drop platform from cask description

--- Casks/yap.rb
+++ Casks/yap.rb
@@ -5,7 +5,7 @@ cask "yap" do
   url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg",
       verified: "github.com/FrigadeHQ/yap/"
   name "Yap"
-  desc "On-device voice dictation for macOS"
+  desc "On-device voice dictation"
   homepage "https://github.com/FrigadeHQ/yap"
 
   depends_on macos: :tahoe
