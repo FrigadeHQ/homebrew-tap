9d9bc174b0b8d0d7ac33345c6d9ce476faf1b71d
christianmat <christian_mathiesen@alumni.brown.edu>
2026-07-24 12:40:29 -0700

    Add Yap cask

---
+++ Casks/yap.rb
@@ -0,0 +1,19 @@
+cask "yap" do
+  version "0.1.0"
+  sha256 "075cff7d38e26667f9e9b1d61a4c34d35402655ba65e71cdc693bb7d004b124b"
+
+  url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg",
+      verified: "github.com/FrigadeHQ/yap/"
+  name "Yap"
+  desc "On-device voice dictation for macOS"
+  homepage "https://github.com/FrigadeHQ/yap"
+
+  depends_on macos: ">= :tahoe"
+
+  app "Yap.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.frigade.Yap.plist",
+    "~/Library/Application Support/Yap",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Frigade Homebrew Tap
+
+Casks for [Frigade](https://frigade.com) open source apps.
+
+## Yap
+
+Blazing-fast, on-device voice dictation for macOS.
+
+```bash
+brew install --cask frigadehq/tap/yap
+```
