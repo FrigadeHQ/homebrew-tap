91654c933a26725c752e76dd2d6afdab1441b95e
Christian Mathiesen <christian@frigade.com>
2026-07-31 13:55:16 -0700

    Update Yap to 0.1.8

--- Casks/yap.rb
+++ Casks/yap.rb
@@ -1,6 +1,6 @@
 cask "yap" do
-  version "0.1.7"
-  sha256 "f683bd7af5b67a36ffe2234d8a6921ea7dcee4778511ed6b799dbe86184d09f8"
+  version "0.1.8"
+  sha256 "a3c7ff8d752ddc0a8f98b0de2255c331bed2ea4484de607f6229ed26a7fdd34a"
 
   url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg",
       verified: "github.com/FrigadeHQ/yap/"
