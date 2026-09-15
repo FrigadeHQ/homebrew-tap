58ca564f587b35b875624373f5ba9b0a5be74da3
Christian Mathiesen <christian@frigade.com>
2026-09-14 17:08:50 -0700

    Update yap to 0.1.12

--- Casks/yap.rb
+++ Casks/yap.rb
@@ -1,6 +1,6 @@
 cask "yap" do
-  version "0.1.11"
-  sha256 "c89e5a7774e9055caef11894ce281ded19a2a223ec1f1813936381b20424c20b"
+  version "0.1.12"
+  sha256 "c600cad9120cbd7ec463d001f002ba3e6cfdf2445577cb957d4166d62824b28e"
 
   url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg",
       verified: "github.com/FrigadeHQ/yap/"
